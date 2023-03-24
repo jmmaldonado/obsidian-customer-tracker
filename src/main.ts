e5336814a95267c8799eef9002754ceba14c7b70
Maldini <jmmaldonado@gmail.com>
2023-03-24 19:54:35 +0100

    Generate customer updates on load and force command

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { CustomerTrackerSettings, CustomerTrackerSettingsTab, DEFAULT_SETTINGS }
 import { Customers, Customer } from 'src/Customers'
 import { CustomerInitiatives, CustomerInitiative } from 'src/CustomerInitiatives'
 
-// Remember to rename these classes and interfaces!
-
-
 
 export default class CustomerTracker extends Plugin {
 	settings: CustomerTrackerSettings;
@@ -36,8 +33,6 @@ export default class CustomerTracker extends Plugin {
 				let customerArea = "";
 				let customerInitiative: CustomerInitiative | undefined;
 
-
-
 				for (const line of lines) {
 
 					if (updateRegex.test(line)) {
@@ -50,10 +45,8 @@ export default class CustomerTracker extends Plugin {
 								//Add the previous initiative, create a new one and add it to the customer object
 								customer.addInitiativeToArea(customerArea, customerInitiative);
 								customerInitiative = new CustomerInitiative(line, customerArea, customer.name);
-								//customer.addInitiativeToArea(customerArea, customerInitiative);
 							} else {
 								customerInitiative = new CustomerInitiative(line, customerArea, customer.name);
-								//customer.addInitiativeToArea(customerArea, customerInitiative);
 							}
 						}
 
@@ -86,7 +79,7 @@ export default class CustomerTracker extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
+		await this.generateCustomers();
 
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Customer tracker', async (evt: MouseEvent) => {
@@ -101,29 +94,24 @@ export default class CustomerTracker extends Plugin {
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		/*
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-		*/
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				//new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'add-customer-tracking-current-note',
+			name: 'Add customer tracking to current note',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				//console.log(editor.getSelection());
+				editor.replaceSelection(this.customers.renderMD());
 			}
 		});
+
+		this.addCommand({
+			id: 'generate-customers',
+			name: 'Reload customer updates',
+			callback: () => {
+				this.generateCustomers();
+			}
+		});
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'open-sample-modal-complex',
@@ -149,12 +137,12 @@ export default class CustomerTracker extends Plugin {
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		/*this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
-		});
+		});*/
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		//this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
