965a3dc8d43c16d16925b12a1c7a5beb44f53a13
Maldini <jmmaldonado@gmail.com>
2023-03-24 16:18:58 +0100

    Added regex to the configuration pane #2

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { CustomerInitiatives, CustomerInitiative } from 'src/CustomerInitiatives
 
 interface CustomerTrackerSettings {
 	customersBaseFolder: string;
+	areaRegex: string;
+	initiativeRegex: string;
+	updateRegex: string;
 
 	//TODO: Future implementation to separate customer areas in different files
 	customerAreasInMainFile: boolean;
@@ -13,6 +16,9 @@ interface CustomerTrackerSettings {
 
 const DEFAULT_SETTINGS: CustomerTrackerSettings = {
 	customersBaseFolder: 'Spaces/Customers/',
+	areaRegex: '^#{1}\\s(.*)',
+	initiativeRegex: '^#{2}\\s(.*)',
+	updateRegex: '^#{4}\\s(.*)', 
 	customerAreasInMainFile: true
 }
 
@@ -29,9 +35,12 @@ export default class CustomerTracker extends Plugin {
 		);
 
 		//TODO: Sacarlo a settings
-		let areaRegex = new RegExp('^#{1}\\s(.*)');
+		/*let areaRegex = new RegExp('^#{1}\\s(.*)');
 		let initiativeRegex = new RegExp('^#{2}\\s(.*)');
-		let updateRegex = new RegExp('^#{4}\\s(.*)');
+		let updateRegex = new RegExp('^#{4}\\s(.*)');*/
+		let areaRegex = new RegExp(this.settings.areaRegex);
+		let initiativeRegex = new RegExp(this.settings.initiativeRegex);
+		let updateRegex = new RegExp(this.settings.updateRegex);
 
 		this.customers = new Customers();
 		const files = this.app.vault.getMarkdownFiles()
@@ -222,6 +231,39 @@ class SampleSettingTab extends PluginSettingTab {
 				.setValue(this.plugin.settings.customersBaseFolder)
 				.onChange(async (value) => {
 					this.plugin.settings.customersBaseFolder = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+		.setName('Area regex')
+		.setDesc('Regex to detect customer areas in a customer note')
+		.addText(text => text
+			.setPlaceholder('^#{1}\\s(.*)')
+			.setValue(this.plugin.settings.areaRegex)
+			.onChange(async (value) => {
+				this.plugin.settings.areaRegex = value;
+				await this.plugin.saveSettings();
+			}));
+
+		new Setting(containerEl)
+			.setName('Initiative regex')
+			.setDesc('Regex to detect customer initiatives in a customer note')
+			.addText(text => text
+				.setPlaceholder('^#{2}\\s(.*)')
+				.setValue(this.plugin.settings.initiativeRegex)
+				.onChange(async (value) => {
+					this.plugin.settings.initiativeRegex = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Update regex')
+			.setDesc('Regex to detect updates in a customer note')
+			.addText(text => text
+				.setPlaceholder('^#{4}\\s(.*)')
+				.setValue(this.plugin.settings.updateRegex)
+				.onChange(async (value) => {
+					this.plugin.settings.updateRegex = value;
 					await this.plugin.saveSettings();
 				}));
 	}
