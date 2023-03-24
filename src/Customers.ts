4f3c876f7faeeb43d71d4f123b5a8f6018e1b94b
Maldini <jmmaldonado@gmail.com>
2023-03-24 12:24:09 +0100

    implemented adding initiatives to an area

--- src/Customers.ts
+++ src/Customers.ts
@@ -19,21 +19,32 @@ export class Customers {
 export class Customer {
     name: string;
     file: string;
-    areas: Map<string, CustomerInitiatives>;
+    #areas: Map<string, CustomerInitiatives>;
 
     public constructor(name: string, file: string) {
         this.name = name;
         this.file = file;
-        this.areas = new Map();
+        this.#areas = new Map();
     }
 
-    public containsArea(area: string): boolean {
-        return this.areas.has(area);
+    public containsArea(areaName: string): boolean {
+        return this.#areas.has(areaName);
+    }
+
+    public addArea(areaName: string) {
+        if (!this.containsArea(areaName))  {
+            let initiatives = new CustomerInitiatives(areaName, this.name);
+            this.#areas.set(areaName, initiatives);
+        }
     }
 
     public getInitiativesFromArea(area: string): CustomerInitiatives | undefined | null {
         if (!this.containsArea(area)) 
             return null;
-        return this.areas.get(area);
+        return this.#areas.get(area);
+    }
+
+    public addInitiativesToArea(area: string, initiatives: CustomerInitiatives) {
+        this.#areas.set(area, initiatives);
     }
 }
