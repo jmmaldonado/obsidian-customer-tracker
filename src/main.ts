686b5b8bb293763548f63180cc1d2b9807bf593b
Maldini <jmmaldonado@gmail.com>
2023-03-26 16:35:42 +0200

    removed unused code

--- src/main.ts
+++ src/main.ts
@@ -102,7 +102,6 @@ export default class CustomerTracker extends Plugin {
 			if (this.customers) {
 				new FilterModal(this.app, this.app.workspace.activeEditor?.editor, this.customers).open();
 			}
-			//new Notice('This is a notice!');
 
 		});
 		// Perform additional things with the ribbon
@@ -110,10 +109,9 @@ export default class CustomerTracker extends Plugin {
 
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'add-customer-tracking-current-note',
-			name: 'Add customer tracking to current note',
+			id: 'add-customer-tracking-summary-current-note',
+			name: 'Add customer tracking summary to current note',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				//console.log(editor.getSelection());
 				editor.replaceSelection(this.customers.renderMD());
 			}
 		});
@@ -126,37 +124,9 @@ export default class CustomerTracker extends Plugin {
 			}
 		});
 
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						//new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new CustomerTrackerSettingsTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		/*this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});*/
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		//this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
