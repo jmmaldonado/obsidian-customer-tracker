ab4e53decd314a5677ae29b29b6934831c76f476
Maldini <jmmaldonado@gmail.com>
2023-04-07 17:25:40 +0200

    Added statistic methods to PeopleUpdates class #23

--- src/PeopleUpdates.ts
+++ src/PeopleUpdates.ts
@@ -7,7 +7,7 @@ export class PeopleUpdates {
     public constructor() {
         this.updates = new Map<string, CustomerInitiatives>();
     }
-    
+
     public addUpdate(update: CustomerUpdate) {
         let initiatives = this.updates.get(update.person);
         if (initiatives) {
@@ -17,5 +17,47 @@ export class PeopleUpdates {
             initiatives.addUpdate(update);
             this.updates.set(update.person, initiatives);
         }
+    }
+
+    public getInitiatives(person: string): CustomerInitiatives | undefined {
+        return this.updates.get(person);
+    }
+
+    public numberOfCustomers(person: string): number {
+        let result = 0;
+        let initiatives = this.getInitiatives(person);
+        if (initiatives) {
+            let customers: string[] = []
+            for (let initiative of initiatives.initiatives.values()) {
+                if (!customers.contains(initiative.customer))
+                    customers.push(initiative.customer);
+            }
+            result = customers.length;
+        }
+        return result;
+    }
+
+    public numberOfInitatives(person: string): number {
+        let result = 0;
+        let initiatives = this.getInitiatives(person);
+        if (initiatives) 
+            result = initiatives.initiatives.size;
+        return result;
+    }
+
+    public numberOfInitiativesByStatus(person: string) : number[] {
+        let result = [0, 0, 0, 0]; //InProgress (""), Won, Lost, Other
+        let initiatives = this.getInitiatives(person);
+        if (initiatives) {
+            for (let initiative of initiatives.initiatives.values()) {
+                switch (initiative.status.toLowerCase()) {
+                    case "": { result[0] += 1; break; }
+                    case "won": { result[1] += 1; break; }
+                    case "lost": { result[2] += 1; break; }
+                    default: { result[3] += 1; break; }
+                }
+            }
+        }
+        return result;
     }
 }
