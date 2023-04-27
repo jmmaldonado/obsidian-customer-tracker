f63d61f8f752ec377bf655afc345e67980f7866f
Maldini <jmmaldonado@gmail.com>
2023-04-27 17:29:01 +0200

    Fixed how myself updates link in summary table #30

--- src/CustomerUpdate.ts
+++ src/CustomerUpdate.ts
@@ -10,15 +10,27 @@ export class CustomerUpdate {
     file: TFile;
     
     public getLinkToUpdateAndPerson(text?: string): string {
-        let clean = this.raw;
-        clean = clean.replace("#####","");
-        clean = clean.replace("#"," ");
-        clean = clean.replace("[[", "");
-        clean = clean.replace("]]", "");
-        if (text == null)
-            return "[[{0}#{1}]] by {2}".format(this.person, clean, this.person);
-        else
-            return "[[{0}#{1}\\|{2}]] by [[{3}]]".format(this.person, clean, text, this.person);
+        if (this.person === "myself") {
+            let clean = this.raw;
+            clean = clean.replace("#####","");
+            clean = clean.replace("#"," ");
+            clean = clean.replace("[[", "");
+            clean = clean.replace("]]", "");
+            if (text == null)
+                return "[[{0}#{1}]] by {2}".format(this.file.basename, clean, this.person);
+            else
+                return "[[{0}#{1}\\|{2}]] by {3}".format(this.file.basename, clean, text, this.person);
+        } else {
+            let clean = this.raw;
+            clean = clean.replace("#####","");
+            clean = clean.replace("#"," ");
+            clean = clean.replace("[[", "");
+            clean = clean.replace("]]", "");
+            if (text == null)
+                return "[[{0}#{1}]] by {2}".format(this.person, clean, this.person);
+            else
+                return "[[{0}#{1}\\|{2}]] by [[{3}]]".format(this.person, clean, text, this.person);
+        }
     }
 
     public getLink(text?: string): string {
